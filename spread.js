49f87baa6af389ed1ac9ea693626e6ac2b3ffb2f
Test95321 <test95321@outlook.com>
2021-04-13 12:01:08 +0400

    First commit

--- spread.js
+++ spread.js
@@ -1,2 +1,21 @@
-const newArray = [...oldArray, 1,2];
-const newObject = {...oldObject, newProp:5}
+const numbers = [1,2,3,4,5]
+const newNumbers =[...numbers, 6,7,8,9,10]
+console.log(newNumbers);
+
+const person = {
+    name: "Max"
+}
+const newPerson = {
+    ...person,
+    age: 28,
+}
+
+console.log(newPerson);
+
+
+
+const filter =  (...args) => {
+    return args
+}
+
+console.log(filter(numbers, newNumbers));
