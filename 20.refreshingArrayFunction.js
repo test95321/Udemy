1a7e78c8595b20cfe65efcac0b64e677ee717530
Test95321 <test95321@outlook.com>
2021-04-25 18:59:53 +0400

    commit

---
+++ 20.refreshingArrayFunction.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => {
+    return num*2;
+})
+
+console.log(numbers);
+console.log(doubleNumArray);
+
+numbers[2] = 8;
+
+console.log(numbers);
+console.log(doubleNumArray);
